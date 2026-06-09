cd1ad46fd8e64fdb3b276ff10875d4b6e8bb8864
anushka jain <anushkaj161007@gmail.com>
2026-06-09 17:03:14 +0530

    Initial commit

---
+++ day9/repeatedchar.java
@@ -0,0 +1,21 @@
+package day9;
+
+import java.util.Scanner;
+
+public class repeatedchar {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        char a = 'A';
+        for(int i=1; i<=n; i++){
+            
+            for(int j =1; j<=i; j++){
+                System.out.print(a);
+                
+            }
+            System.out.println(" ");
+            a++;
+        }
+        sc.close();
+    }
+}
